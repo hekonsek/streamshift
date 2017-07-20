7b630b800a0cda3f9c97faca21c758a03f96d6bc
ajurasz <arkadiusz.jurasz@gmail.com>
2017-07-20 10:16:17 +0200

    Fix Kafka advertised properties.

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sed -i "/^zookeeper.connect=/s/=.*/=${ZOOKEEPER_SERVICE_HOST}/" /usr/kafka_2.12-0.10.2.1/config/server.properties
-sed -i "/^advertised.port=/s/=.*/=9092/" /usr/kafka_2.12-0.10.2.1/config/server.properties
+echo "advertised.host.name=${KAFKA_SERVICE_HOST}" >> /usr/kafka_2.12-0.10.2.1/config/server.properties
+echo "advertised.port=9092" >> /usr/kafka_2.12-0.10.2.1/config/server.properties
 
 /usr/kafka_2.12-0.10.2.1/bin/kafka-server-start.sh /usr/kafka_2.12-0.10.2.1/config/server.properties
+/usr/kafka_2.12-0.10.2.1/bin/kafka-rest-start.sh /usr/kafka_2.12-0.10.2.1/config/server.properties
