aa39c35fa705ec6dcaee0b1c54df629305c78c20
ajurasz <arkadiusz.jurasz@gmail.com>
2017-07-24 12:34:36 +0200

    Setup volume in docker images.

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sed -i "/^zookeeper.connect=/s/=.*/=${ZOOKEEPER_SERVICE_HOST}/" /usr/kafka_2.12-0.10.2.1/config/server.properties
+sed -i "/^log.dirs=/s|=.*|=/kafka|" /usr/kafka_2.12-0.10.2.1/config/server.properties
 echo "advertised.host.name=${KAFKA_SERVICE_HOST}" >> /usr/kafka_2.12-0.10.2.1/config/server.properties
 echo "advertised.port=9092" >> /usr/kafka_2.12-0.10.2.1/config/server.properties
 
